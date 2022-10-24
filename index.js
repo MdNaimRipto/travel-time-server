ff9719a2866e25067ebeabe3a7692d668b5479f0
MdNaimRipto <naimurtsc567@gmail.com>
2022-10-24 08:22:36 +0600

    hotel section and map link added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 
 const locations = require("./data/locations.json")
+const allLocation = require("./data/allLocations.json")
 
 app.get("/", (req, res) => {
     res.send("Server is Running")
@@ -20,6 +21,16 @@ app.get("/location/:id", (req, res) => {
     const id = req.params.id;
     const location = locations.find(lc => lc.id == id)
     res.send(location)
+})
+
+app.get("/allLocation", (req, res) => {
+    res.send(allLocation)
+})
+
+app.get("/allLocation/:id", (req, res) => {
+    const id = req.params.id
+    const singleLocation = allLocation.find(location => location.id == id)
+    res.send(singleLocation)
 })
 
 app.listen(port, () => {
